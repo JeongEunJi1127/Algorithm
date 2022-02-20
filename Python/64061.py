5d7825167c467c0f0de70a456b155b317c96f048
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2022-02-20 16:28:47 +0900

    [Update] 64061

---
+++ Python/64061.py
@@ -0,0 +1,52 @@
+from pprint import pprint
+
+def solution(board, moves):
+    answer = 0
+    sol = []
+
+    # pprint(board)
+
+    for move in moves:  # moves = [1,5,3,5,1,2,1,4]
+        for i in range(len(board)):
+            # print(board[i][move-1])
+            if board[i][move-1] != 0:
+                sol.append(board[i][move-1])
+                board[i][move-1] = 0
+                pprint(board)
+                print(sol)
+                print(answer)
+
+                if len(sol) >= 2:
+                    if sol[-1] == sol[-2]:
+                        answer+=2
+                        sol.pop()
+                        sol.pop()
+
+                break
+
+            # print(board[move])
+
+    return answer
+
+# stack, deque 사용해보기!
+print(solution(([0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]),[1,5,3,5,1,2,1,4]) )
+
+
+# from pprint import pprint
+#
+# def solution(board, moves):
+#     answer = 0
+#     sol = []
+#
+#     pprint(board)
+#     print( )
+#
+#     for move in moves:
+#         for i in range(len(board)):
+#             for j in range(len(board[i])):
+#                 sol.append(board[j][move - 1])
+#                 print(sol)
+#             sol = []
+#
+#     return answer
+#
