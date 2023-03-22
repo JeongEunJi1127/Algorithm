10488b6afc43a09bd02a6af5f59faf4e1bea0609
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2023-03-22 20:45:34 +0900

    [Update] 2023.03.22

--- Baekjoon/2023.03.22/10988.py
+++ Baekjoon/2023.03.22/10988.py
@@ -1,18 +1,8 @@
 def is_palindrome(ipt):
-    i = len(ipt) // 2
-    if len(ipt) % 2 != 0:  
-        j = ipt[0:i]
-        if j[::-1] == ipt[i+1:len(ipt)]:
-            return True
-        else:
-            return False
+    if ipt[::-1] == ipt:
+        return True
     else:
-        j = ipt[0:i-1]
-        if j[::-1] == ipt[i+1:len(ipt)]:
-            return True
-        else:
-            return False
-    
+        return False
 s = input()
 
 if is_palindrome(s):
