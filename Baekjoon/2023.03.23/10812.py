1f9019d046886b67c17a33040bca6bcb040b5422
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2023-03-23 17:23:20 +0900

    [Upload] 2023.03.23

---
+++ Baekjoon/2023.03.23/10812.py
@@ -0,0 +1,13 @@
+n,m = map(int,input().split())
+b = []
+
+for i in range(n):
+    b.append(i+1)
+print(b)
+
+for _ in range(m):
+    i,j,k = map(int,input().split())
+    b = b[0:i-1]+b[k-1:j]+b[i-1:k-1]+b[j:]
+
+for i in b:
+    print(i,end=" ")
