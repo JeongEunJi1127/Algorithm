634f72398e93f87968b4cf1b1ae3715b95ac79ae
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2023-10-25 16:22:08 +0900

    [Update] file modified

--- Baekjoon/2023.10.23/2751.py
+++ Baekjoon/2023.10.23/2751.py
@@ -1,9 +1,11 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 lst = []
 
 for _ in range(n):
-    lst.append(int(input()))
+    lst.append(int(sys.stdin.readline()))
 
 lst.sort()
+
 for i in lst:
     print(i)
