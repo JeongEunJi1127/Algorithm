ddb073d489b8851138f98c348d7f472b04fa8d75
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-01-03 18:14:01 +0900

    [Update] 2024.01.03

--- Baekjoon/2024.01.03/24511.py
+++ Baekjoon/2024.01.03/24511.py
@@ -15,10 +15,10 @@ queuestack = deque()
 
 for i in range(n):
     # 스택 고려 x
-    if a[i] == 0: # 큐면
+    if a[i] == 0: # 큐인 데이터만 다룸
         queuestack.appendleft(b[i])
-
-if len(queuestack) == 0:
+# 큐인 데이터의 popleft 값이 출력된 후 (큐이기 때문에) 입력되는 c의 수열 값이 출력된다
+if len(queuestack) == 0: # 배열의 형식이 모두 스택일 경우 입력되는 수열 c만 출력하면 됨.
     print(*c)
 else:
     for i in range(m):
