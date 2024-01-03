d3d9d2b77afdb190cbec82baaf7c91af77136717
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-01-03 18:53:28 +0900

    [Update] 2024.01.03

---
+++ Baekjoon/2024.01.03/11047.py
@@ -0,0 +1,16 @@
+import sys 
+n,k = map(int,sys.stdin.readline().split())
+prices = []
+for _ in range(n):
+    prices.append(int(sys.stdin.readline()))
+prices.reverse()
+ans = 0
+for price in prices:
+    if k == 0:
+        break
+    if k >= price:
+        i = k//price
+        ans += i
+        k = k- (price * i)
+
+print(ans) 
