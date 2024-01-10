f4efb700c68c91545bb8cb41c003ba5e473c2fcc
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-01-10 19:31:05 +0900

    [Update] 2024.01.10

---
+++ Baekjoon/2024.01.10/10870.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+
+def fibonacci(num):
+    x0 = 0
+    x1 = 1
+    fibo = 0
+    while num>1:
+        fibo = x0+x1
+        x0 = x1
+        x1 = fibo
+        num-=1
+    return fibo
+
+print(fibonacci(n))
