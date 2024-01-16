3b89fcbe074ce5293f8da70e4512f7b7c12dd908
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-01-16 13:44:03 +0900

    [Update] 2024.01.16

---
+++ Baekjoon/2024.01.16/11722.py
@@ -0,0 +1,9 @@
+a = int(input())
+num = list(map(int,input().split()))
+dp = [1] * a
+
+for i in range(a):
+    for j in range(i):
+        if num[j] > num[i]:
+            dp[i] = max(dp[i],dp[j]+1)
+print(max(dp))
