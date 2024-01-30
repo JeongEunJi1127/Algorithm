0802cab1d431d84cc04dd751747c15130572f5e5
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-01-30 16:58:16 +0900

    [Update]] 2024.01.30

---
+++ Baekjoon/2024/2024.01.30/11404.py
@@ -0,0 +1,18 @@
+n = int(input())
+m = int(input())
+lst = [[int(1e9) for _ in range(n)] for _ in range(n)]
+
+for i in range(n):
+    lst[i][i] = 0
+
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    lst[a-1][b-1] = min(lst[a-1][b-1],c)
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            lst[j][k] = min(lst[j][k], lst[j][i]+lst[i][k])
+
+for i in lst:
+    print(*i)
