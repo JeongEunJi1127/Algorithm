da9ff466c8c284e166e43178b6d3566de963fb74
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-02-27 15:45:07 +0900

    [Update] 다이나믹 프로그래밍

---
+++ 다이나믹 프로그래밍/정수 삼각형.py
@@ -0,0 +1,10 @@
+def solution(triangle):
+    for i in range(1,len(triangle)):
+        for j in range(i+1):
+            max_val = 0
+            if j-1 >=0:
+                max_val = max(max_val,triangle[i][j]+triangle[i-1][j-1])
+            if j < i:
+                max_val = max(max_val,triangle[i][j]+triangle[i-1][j])
+            triangle[i][j] = max_val
+    return max(max(triangle))
