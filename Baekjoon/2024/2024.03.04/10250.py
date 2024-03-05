ec5fd9726a52953fe227b045118716906b651a21
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-03-05 19:06:50 +0900

    [Update] 다이나믹 프로그래밍

--- Baekjoon/2024/2024.03.04/10250.py
+++ Baekjoon/2024/2024.03.04/10250.py
@@ -2,6 +2,10 @@ t = int(input())
 
 for _ in range(t):
     h,w,n = map(int,input().split())
-    ans = (n%h) * 100 + (n//h) + 1
-    print(ans)
     
+    floor = (n%h) * 100
+    door = (n//h) + 1
+    if floor == 0:
+        floor = h*100
+        door -= 1
+    print(floor+door)
