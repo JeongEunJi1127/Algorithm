c9e52a3ed8c518402c46df0d66e61804c28bb99d
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-03-29 18:36:45 +0900

    [Update] 구현

--- 알고리즘 분류/구현/16926.py
+++ 알고리즘 분류/구현/16926.py
@@ -1,49 +1,48 @@
+import sys
+input = sys.stdin.readline
 n,m,r = map(int,input().split())
 array = []
-# 하우상좌
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
 
 for _ in range(n):
     array.append(list(map(int,input().split())))
 
 def rotate(array):
-    for i in range(min(n,m)//2):
-        x,y = i,i
-        # 맨 처음 값 저장
-        value = array[x][y]
+    for _ in range(r):
+        for i in range(min(n,m)//2):
+            x,y = i,i
+            # 맨 처음 값 저장
+            value = array[x][y]
 
-        # 왼쪽
-        for j in range(i+1,n-i):
-            x = j
-            tmp = array[x][y]
-            array[x][y] = value
-            value = tmp
+            # 왼쪽
+            for j in range(i+1,n-i):
+                x = j
+                tmp = array[x][y]
+                array[x][y] = value
+                value = tmp
 
-        # 아래쪽
-        for j in range(i+1, m-i): 
-            y = j
-            temp = array[x][y]
-            array[x][y] = value
-            value = temp
+            # 아래쪽
+            for j in range(i+1, m-i): 
+                y = j
+                tmp = array[x][y]
+                array[x][y] = value
+                value = tmp
 
-        # 오른쪽
-        for j in range(i+1, n-i): 
-            x = n - j - 1 
-            temp = array[x][y] 
-            array[x][y] = value
-            value = temp
+            # 오른쪽
+            for j in range(i+1, n-i): 
+                x = n - j - 1 
+                tmp = array[x][y] 
+                array[x][y] = value
+                value = tmp
 
-        # 위
-        for j in range(i+1, m-i):
-            y = n - j - 1
-            temp = array[x][y]
-            array[x][y] = value
-            value = temp
+            # 위
+            for j in range(i+1, m-i):
+                y = m - j - 1
+                tmp = array[x][y]
+                array[x][y] = value
+                value = tmp
     return array
 
-for _ in range(r):
-    rotate(array)
+rotate(array)
 
 for i in array:
     print(*i)
