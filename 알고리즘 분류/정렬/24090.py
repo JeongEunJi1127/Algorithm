e8ced7126c940185ff5c7b091412130c3321df09
JeongEunJi1127 <43170505+JeongEunJi1127@users.noreply.github.com>
2024-04-04 16:24:10 +0900

    [Update] 정렬

--- 알고리즘 분류/정렬/24090.py
+++ 알고리즘 분류/정렬/24090.py
@@ -3,38 +3,37 @@ sys.setrecursionlimit(int(1e4))
 input = sys.stdin.readline
 
 def partition(array,start,end):
-    global lst
+    global cnt
     pivot = array[end]
     i = start-1
     for j in range(start,end): 
         if array[j] <= pivot: 
             i +=1
             array[i], array[j] = array[j], array[i]
-            lst.append([array[i], array[j]])
+            cnt += 1
+            if cnt == k:
+                print(array[i],array[j])
 
     if i+1 != end:
         array[i+1], array[end] = array[end], array[i+1]
-        lst.append([array[i+1], array[end]])
-
+        cnt += 1
+        if cnt == k:
+            print(array[i+1],array[end])
     return i+1
 
 def quickSort(array,start,end):
     if start >= end:
         return
     q = partition(array,start,end)
-    quickSort(array,start, q-1)
-    quickSort(array,q+1, end)
+    quickSort(array,start,q-1)
+    quickSort(array,q+1,end)
 
 
 n,k = map(int,input().split())
 array = list(map(int,input().split()))
-lst = []
+cnt = 0
 
 quickSort(array,0,n-1)
 
-if len(lst) < k:
+if cnt < k:
     print(-1)
-else:
-    s = lst[k-1]
-    s.sort()
-    print(*s)
