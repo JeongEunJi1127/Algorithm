0f5044cf9b79f413a62486ee016d332d052a0ffd
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-01-02 17:09:38 +0900

    [Gold I] Title: K번째 수, Time: 364 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,19 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def countNum(idx, n):
+def Add(idx, n):
     cnt = 0
     for i in range(1, n+1):
-        cnt += min(idx // i, n)
+        cnt += min(idx//i, n)
     return cnt
 
-def binarySearch(n, k):
+def BinarySearch(n, k):
     left = 1
     right = k
 
     while left <= right:
         mid = (left + right) // 2
-        if countNum(mid, n) < k:
+        if Add(mid, n) < k:
             left = mid + 1
         else:
             right = mid - 1
@@ -22,5 +22,5 @@ def binarySearch(n, k):
 n = int(input())
 k = int(input())
 
-answer = binarySearch(n, k)
+answer = BinarySearch(n, k)
 print(answer)
