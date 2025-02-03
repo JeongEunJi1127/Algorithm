583a1c5d656d23b843e29a9fde9f169e3c2334f1
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-02-03 20:20:18 +0900

    [Gold V] Title: Z, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,30 @@
+# 보고 품
+n, r, c = map(int, input().split())
+
+ans = 0
+
+while n != 0:
+
+	n -= 1
+
+	# 제2사분면
+	if r < 2 ** n and c < 2 ** n:
+		ans += ( 2 ** n ) * ( 2 ** n ) * 0
+
+	# 제1사분면
+	elif r < 2 ** n and c >= 2 ** n: 
+		ans += ( 2 ** n ) * ( 2 ** n ) * 1
+		c -= ( 2 ** n )
+        
+	# 제3사분면    
+	elif r >= 2 ** n and c < 2 ** n: 
+		ans += ( 2 ** n ) * ( 2 ** n ) * 2
+		r -= ( 2 ** n )
+        
+	# 제4사분면    
+	else:
+		ans += ( 2 ** n ) * ( 2 ** n ) * 3
+		r -= ( 2 ** n )
+		c -= ( 2 ** n )
+    
+print(ans)
