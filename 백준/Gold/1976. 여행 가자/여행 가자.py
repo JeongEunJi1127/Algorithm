79bc3f833b49fbf62be138a0d811fc0771d90594
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-04-16 15:27:45 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,39 +1,41 @@
+# 15:05 ~ (다시 품)
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-parent = [i for i in range(n+1)] 
-graph = []
+parent = [i for i in range(n+1)]
 
-# 노드 n의 루트노드 return
-def find(n):
-    if parent[n] != n:                  
-        parent[n] = find(parent[n])     
-    return parent[n]                    
+def find(a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
 
-# a, b가 다른 집합에 속해있을 때 같은 집합으로 합치는 함수
-def union(a, b):
-    a = find(a)         
-    b = find(b)         
-    if a < b:           
-        parent[b] = a   # b의 부모노드를 a로 설정
-    else:               
-        parent[a] = b  # a의 부모노드를 b로 설정
+def union(a,b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 def solution():
-    for i in range(1,n+1):
-        link_info = list(map(int,input().split()))
-        for j in range(1,n+1):
-            if link_info[j-1] == 1: # 만약 연결되어있다면 union
-                union(i,j)
+    for i in range(n):
+        informations = list(map(int,input().split()))
+        for j in range(n):
+            if informations[j] == 1:
+                union(i+1,j+1)
 
-    travel_plan = list(map(int,input().split()))
-    prev = travel_plan[0]
-    for i in range(1,m):
-        if find(travel_plan[i]) != find(prev):
+    plan = list(map(int,input().split()))
+    root = parent[plan[0]]
+
+    for i in plan:
+        if find(i) != root:
             print("NO")
             exit()
-        prev = travel_plan[i]
+
     print("YES")
+
 solution()
