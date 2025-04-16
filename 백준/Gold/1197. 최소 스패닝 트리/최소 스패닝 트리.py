82781b8cc72eec8703c143c1322bc92da5e4082d
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-04-16 14:56:59 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 260 ms, Memory: 50664 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,38 +1,45 @@
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(100000)
+# 14:31 ~ 
+# 최소 스패닝 트리 
+# 주어진 그래프의 모든 정점들을 연결하는 부분 그래프 중에서 그 가중치의 합이 최소인 트리
+# 사이클이 존재하지 않는 트리
+# 유니온 파인드 알고리즘
 
-def find(x):
-    if x != parent[x]:
-        parent[x] = find(parent[x])
-    return parent[x]
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+v,e = map(int,input().split())
+parent = [i for i in range(v+1)]
+informations = []
 
 def union(a,b):
-    a = find(a)
+    a = find(a) # 부모 노드 값 설정
     b = find(b)
 
-    if a<b:
+    if a != b: 
         parent[b] = a
-    else:
-        parent[a] = b
+        return True
+    return False
+
+def find(a):
+    if parent[a] != a:
+        parent[a] = find(parent[a])
+    return parent[a]
+
 
 def solution():
     answer = 0
+    for _ in range(e):
+        a,b,c = map(int,input().split())
+        informations.append((c,a,b))
 
-    for c,a,b in graph:
-        if find(a) != find(b):
-            union(a,b)
+    informations.sort()
+
+    for c,a,b in informations:
+        if union(a,b):
             answer += c
-    return answer
 
-v,e = map(int,input().split())
-graph = []
-parent = [i for i in range(v+1)]
+    print(answer)
+        
 
-for _ in range(e):
-    a,b,c = map(int,input().split())
-    graph.append((c,a,b))
-
-graph.sort()
-
-print(solution())
+solution()
