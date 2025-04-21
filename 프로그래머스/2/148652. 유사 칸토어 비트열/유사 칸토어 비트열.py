432963b48512f3a8e30e16675f66c1a900b2aded
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-04-21 17:44:21 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 5957.84 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -0,0 +1,16 @@
+def is_one(index):
+    while index >= 5:
+        if (index - 2) % 5 == 0:
+            return False
+        index //= 5
+
+    return index != 2
+
+
+def solution(n, l, r):
+    answer = 0
+    for i in range(l-1,r):
+        if is_one(i):
+            answer += 1
+        
+    return answer
