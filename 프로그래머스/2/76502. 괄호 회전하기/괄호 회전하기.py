061e7e999240322024836f9381fd30fc1ce3e3d1
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-04-21 19:00:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 61.46 ms, Memory: 9.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def solution(s):
+    answer = 0
+    n = len(s)
+    open_string = ["[","(","{"]
+    
+    for i in range(n):
+        new_string = s[i:]+s[:i]   
+        
+        stack = deque()
+        is_valid = True
+    
+        for j in new_string:
+            if j in open_string:
+                stack.append(j)
+            elif j == "]":
+                if not stack or stack.pop() != "[":
+                    is_valid = False
+                    break         
+            elif j == "}":
+                if not stack or stack.pop() != "{":
+                    is_valid = False
+                    break  
+            elif j == ")":
+                if not stack or stack.pop() != "(":
+                    is_valid = False
+                    break  
+            
+        if is_valid and not stack:
+            answer += 1
+
+    return answer
