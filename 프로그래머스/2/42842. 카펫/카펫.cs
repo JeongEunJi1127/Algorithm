5eb8949307b494b9e2f1cbc9df434b497c9bac3c
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-08-18 17:24:55 +0900

    [level 2] Title: 카펫, Time: 0.28 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cs
@@ -0,0 +1,25 @@
+// 17:19 ~ 17:30
+
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        int total = brown + yellow;
+        
+        for (int height = 3; height <= total; height++) {
+            if (total % height != 0) continue;
+            int width = total / height;
+
+            if ((width-2) * (height-2) == yellow) {
+                answer[0] = width;
+                answer[1] = height;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
