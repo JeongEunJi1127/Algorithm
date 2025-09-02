516e7b42feb24abe54ecb4061e83ce2c41541a76
정은지 <43170505+JeongEunJi1127@users.noreply.github.com>
2025-09-02 18:35:55 +0900

    [level 0] Title: 문자열 돌리기, Time: 16.17 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+        s = Console.ReadLine();
+        
+        foreach(char i in s){
+            Console.WriteLine(i);
+        }
+        
+
+    }
+}
